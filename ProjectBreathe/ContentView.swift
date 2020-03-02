177de70340e5e8341614d17cc2918e3d440bda44
Dan O'Leary <olearydaniel@me.com>
2020-03-02 09:45:01 -0600

    Initial Commit

---
+++ ProjectBreathe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectBreathe
+//
+//  Created by Daniel O'Leary on 3/2/20.
+//  Copyright © 2020 Impulse Coupled Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
